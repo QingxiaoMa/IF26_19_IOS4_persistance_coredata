753480a6fee6c1abbd5133633d1268bd834ccc18
MA Qingxiao <maqingxi@wifi1805.utt.fr>
2019-12-03 14:22:28 +0100

    Initial Commit

---
+++ IF26_19_IOS4_persistance_coredata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IF26_19_IOS4_persistance_coredata
+//
+//  Created by MA Qingxiao on 03/12/2019.
+//  Copyright © 2019 IF26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
